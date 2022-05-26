2c471dde12ea0a9ae6cf02bf4a09f6013f04540a
maxisoft <maxisoft.eu@gmail.com>
2022-05-26 17:39:00 +0200

    Added PairFilter

---
+++ CryptoDumper.Tests/Pairs/PairFilterTest.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using CryptoDumper.Ftx;
+using CryptoDumper.Http;
+using CryptoDumper.Pairs;
+using Xunit;
+
+namespace CryptoDumper.Tests.Pairs;
+
+public class PairFilterTest
+{
+    [Fact]
+    public void TestPairFilter_OneItem()
+    {
+        {
+            var pf = new PairFilter();
+            pf.AddAll("BTC-PERP");
+
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.False(pf.Match("eth-perp"));
+            Assert.False(pf.Match("eth-usd"));
+            Assert.False(pf.Match("eth/usd"));
+        }
+    }
+
+    [Fact]
+    public void TestPairFilter_Regex()
+    {
+        {
+            var pf = new PairFilter();
+            pf.AddAll("BTC.*$");
+
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.True(pf.Match("btc/usd"));
+            Assert.True(pf.Match("btc/eth"));
+            Assert.True(pf.Match("btc13ZDSQDq54"));
+            Assert.False(pf.Match("eth-perp"));
+            Assert.False(pf.Match("eth-usd"));
+            Assert.False(pf.Match("eth/usd"));
+        }
+    }
+
+
+    [Fact]
+    public void TestPairFilter_items()
+    {
+        {
+            var pf = new PairFilter();
+            pf.AddAll("BTC-PERP;ETH-PERP");
+
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.False(pf.Match("btc/usd"));
+            Assert.False(pf.Match("btc/eth"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.False(pf.Match("eth-usd"));
+            Assert.False(pf.Match("eth/usd"));
+        }
+
+
+        {
+            var pf = new PairFilter();
+            pf.AddAll(" BTC-PERP; ETH-PERP ");
+
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.False(pf.Match("btc/usd"));
+            Assert.False(pf.Match("btc/eth"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.False(pf.Match("eth-usd"));
+            Assert.False(pf.Match("eth/usd"));
+        }
+
+        {
+            var pf = new PairFilter();
+            pf.AddAll(@" BTC-PERP
+ ETH-PERP 
+BTC-PERP
+
+BTC-PERP
+BTC-PERP
+BTC-PERP");
+
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.False(pf.Match("btc/usd"));
+            Assert.False(pf.Match("btc/eth"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.False(pf.Match("eth-usd"));
+            Assert.False(pf.Match("eth/usd"));
+        }
+    }
+
+    [Fact]
+    public void TestPairFilter_Empty()
+    {
+        {
+            var pf = new PairFilter();
+
+            // must match all
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.True(pf.Match("eth-usd"));
+            Assert.True(pf.Match("eth/usd"));
+        }
+
+        {
+            var pf = new PairFilter();
+            pf.AddAll("");
+
+            // must match all
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.True(pf.Match("eth-usd"));
+            Assert.True(pf.Match("eth/usd"));
+        }
+
+        {
+            var pf = new PairFilter();
+            pf.AddAll("#BTC-PERP"); // commented
+
+            // must match all
+            Assert.True(pf.Match("BTC-PERP"));
+            Assert.True(pf.Match("btc-perp"));
+            Assert.True(pf.Match("eth-perp"));
+            Assert.True(pf.Match("eth-usd"));
+            Assert.True(pf.Match("eth/usd"));
+        }
+    }
+
+    [Fact]
+    public void TestPairFilter_multiline()
+    {
+        var data = @"
+    BTC:USDT
+BTCUSD
+BTC:USDT
+ETHUSD.{0,2}
+_LUNA
+#COMMENT
+# COMMENT
+##COMMENT2
+//COMMENTPAR
+";
+        var pf = new PairFilter();
+        pf.AddAll(data);
+
+        Assert.True(pf.Match("BTCUSD"));
+        Assert.True(pf.Match("btcUsD"));
+        Assert.True(pf.Match("BTC:USDT"));
+        Assert.True(pf.Match("_LUNA"));
+        Assert.False(pf.Match("LUNA"));
+        Assert.True(pf.Match("ethusdt"));
+        Assert.True(pf.Match("ethusd"));
+        Assert.True(pf.Match("ethusdc"));
+        Assert.False(pf.Match("ethdai"));
+        
+        Assert.False(pf.Match("COMMENT"));
+        Assert.False(pf.Match("#COMMENT"));
+        Assert.False(pf.Match("//COMMENTPAR"));
+        Assert.False(pf.Match("COMMENTPAR"));
+    }
+}
---
+++ CryptoDumper/Pairs/PairFilter.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.RegularExpressions;
+using Maxisoft.Utils.Collections.Dictionaries;
+using Maxisoft.Utils.Collections.Dictionaries.Specialized;
+using Maxisoft.Utils.Collections.LinkedLists;
+using Maxisoft.Utils.Empties;
+
+namespace CryptoDumper.Pairs;
+
+public struct PairFilterEntry : IEquatable<PairFilterEntry>
+{
+    public readonly string Text;
+    public readonly Regex? Regex;
+
+    internal PairFilterEntry(string text, Regex? regex = null)
+    {
+        Text = text;
+        Regex = regex;
+    }
+
+    public bool IsRegex => Regex is { };
+
+    public bool Match(string s)
+    {
+        if (Regex is { })
+        {
+            return Regex!.IsMatch(s);
+        }
+
+        return Text == s;
+    }
+
+    # region IEquatable
+
+    public bool Equals(PairFilterEntry other) => Text == other.Text && Equals(Regex, other.Regex);
+
+    public override bool Equals(object? obj) => obj is PairFilterEntry other && Equals(other);
+
+    public override int GetHashCode() => HashCode.Combine(Text, Regex);
+
+    public static bool operator ==(PairFilterEntry left, PairFilterEntry right) => left.Equals(right);
+
+    public static bool operator !=(PairFilterEntry left, PairFilterEntry right) => !left.Equals(right);
+
+    # endregion
+}
+
+public class PairFilter
+{
+    public static readonly Regex DetectRegex = new Regex(@"^[a-zA-Z][\w:/-]+$",
+        RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+    private OrderedDequeDictionary<string, EmptyStruct> _pairsSet = new OrderedDequeDictionary<string, EmptyStruct>(StringComparer.InvariantCultureIgnoreCase);
+
+    private LinkedListAsIList<PairFilterEntry> _entries = new LinkedListAsIList<PairFilterEntry>();
+    private static readonly char[] Separator = new[] { '\r', '\n', ';' };
+
+
+    public void AddAll(string input)
+    {
+        var es = new EmptyStruct();
+        foreach (var s in input.Split(Separator,
+                     StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (s.StartsWith('#') || s.StartsWith("//", StringComparison.InvariantCulture)) continue;
+            if (_pairsSet.TryAdd(s, es) && !DetectRegex.IsMatch(s))
+            {
+                _entries.AddLast(new PairFilterEntry(s,
+                    new Regex(s,
+                        RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant |
+                        RegexOptions.IgnoreCase)));
+            }
+        }
+    }
+
+    public bool Match(string input)
+    {
+        if (_pairsSet.ContainsKey(input)) return true;
+        var node = _entries.First;
+        while (node is {})
+        {
+            if (node.Value.Match(input))
+            {
+                // lru behavior
+                _entries.Remove(node);
+                _entries.AddFirst(node.Value);
+                return true;
+            }
+
+            node = node.Next;
+        }
+
+        return !_pairsSet.Any(); // 
+    }
+}
