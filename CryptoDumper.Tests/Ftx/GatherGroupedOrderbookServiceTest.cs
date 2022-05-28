95fb172c166fc9fc8d8901587a318fdb72c8d67b
maxisoft <maxisoft.eu@gmail.com>
2022-05-28 11:25:45 +0200

    Improved Testing code

--- CryptoDumper.Tests/Ftx/GatherGroupedOrderbookServiceTest.cs
+++ CryptoDumper.Tests/Ftx/GatherGroupedOrderbookServiceTest.cs
@@ -18,42 +18,43 @@ using Xunit;
 
 namespace CryptoDumper.Tests.Ftx;
 
-public class GatherGroupedOrderbookServiceTest: IDisposable
+public class GatherGroupedOrderbookServiceTest : IDisposable
 {
-    private readonly string tmpPath;
+    private readonly string _tmpPath;
     private readonly Container _container;
-    private readonly Mock<StaticPairFilterLoader> PairFilterLoaderMock;
-    private readonly Mock<GroupedOrderBookHandler> GroupedObHandlerMock;
+    private readonly Mock<StaticPairFilterLoader> _pairFilterLoaderMock;
+    private readonly Mock<GroupedOrderBookHandler> _groupedObHandlerMock;
 
     public GatherGroupedOrderbookServiceTest()
     {
-        tmpPath = Path.GetTempPath();
-        tmpPath = Path.Combine(tmpPath, new Guid().ToString());
-        Directory.CreateDirectory(tmpPath);
-        PairFilterLoaderMock = new Mock<StaticPairFilterLoader> {CallBase = true};
-        GroupedObHandlerMock = new Mock<GroupedOrderBookHandler> {CallBase = true};
+        _tmpPath = Path.GetTempPath();
+        _tmpPath = Path.Combine(_tmpPath, new Guid().ToString());
+        Directory.CreateDirectory(_tmpPath);
+        _pairFilterLoaderMock = new Mock<StaticPairFilterLoader> { CallBase = true };
+        _groupedObHandlerMock = new Mock<GroupedOrderBookHandler> { CallBase = true };
         _container = new ContainerFactory().CreateContainer(new CreateContainerOptions()
         {
             ScanForPlugins = false,
             ConfigurationServiceOptions = new ConfigurationServiceOptions()
             {
-                WorkingDirectory = tmpPath,
-                DefaultBasePath = tmpPath,
+                WorkingDirectory = _tmpPath,
+                DefaultBasePath = _tmpPath,
                 ScanForEnvConfig = false,
                 DefaultConfig = new OrderedDictionary<string, string>()
             },
-            
-            PostConfigure = c => {
+
+            PostConfigure = c =>
+            {
                 // RemoveAll doesnt work in lamar ?
                 // https://jasperfx.github.io/lamar/documentation/ioc/registration/changing-configuration-at-runtime/
                 c.RemoveAll<IGroupedOrderbookHandler>();
                 c.RemoveAll<IPairFilterLoader>();
-                c.ForSingletonOf<IPairFilterLoader>().Use(PairFilterLoaderMock.Object);
-                c.ForSingletonOf<Mock<GroupedOrderBookHandler>>().Use(GroupedObHandlerMock);
-                c.ForSingletonOf<IGroupedOrderbookHandler>().Use(GroupedObHandlerMock.Object);
+                c.ForSingletonOf<IPairFilterLoader>().Use(_pairFilterLoaderMock.Object);
+                c.ForSingletonOf<Mock<GroupedOrderBookHandler>>().Use(_groupedObHandlerMock);
+                c.ForSingletonOf<IGroupedOrderbookHandler>().Use(_groupedObHandlerMock.Object);
             }
         });
-        
+
         foreach (var handler in _container.GetAllInstances<IGroupedOrderbookHandler>())
         {
             handler.Disabled = true;
@@ -67,7 +68,8 @@ public class GatherGroupedOrderbookServiceTest: IDisposable
         internal List<IReadOnlyCollection<GroupedOrderbookDetails>> ReplayMemory =
             new List<IReadOnlyCollection<GroupedOrderbookDetails>>();
 
-        public Task Handle(IReadOnlyCollection<GroupedOrderbookDetails> orderbooks, CancellationToken cancellationToken)
+        public virtual Task Handle(IReadOnlyCollection<GroupedOrderbookDetails> orderbooks,
+            CancellationToken cancellationToken)
         {
             ReplayMemory.Add(orderbooks.ToArray());
             return Task.CompletedTask;
@@ -75,18 +77,23 @@ public class GatherGroupedOrderbookServiceTest: IDisposable
 
         public bool Disabled { get; set; }
     }
-    
+
     public class StaticPairFilterLoader : IPairFilterLoader
     {
-        internal static readonly Mock<PairFilter> PairFilter;
+        internal readonly Mock<PairFilter> PairFilter;
 
-        static StaticPairFilterLoader()
+        public StaticPairFilterLoader()
         {
-            PairFilter = new Mock<PairFilter>() {CallBase = true};
-            PairFilter.Object.AddAll("BTC-PERP; ETH/USDT");
+            PairFilter = new Mock<PairFilter>() { CallBase = true };
         }
 
-        public virtual ValueTask<IPairFilter> GetPairFilterAsync(string name, CancellationToken cancellationToken = default)
+        public void AddAll(string input)
+        {
+            PairFilter.Object.AddAll(input);
+        }
+
+        public virtual ValueTask<IPairFilter> GetPairFilterAsync(string name,
+            CancellationToken cancellationToken = default)
         {
             return ValueTask.FromResult<IPairFilter>(PairFilter.Object);
         }
@@ -94,28 +101,39 @@ public class GatherGroupedOrderbookServiceTest: IDisposable
 
     public void Dispose()
     {
-        Directory.Delete(tmpPath, true);
+        Directory.Delete(_tmpPath, true);
         _container.Dispose();
     }
 
     [Fact]
     public async void Test_NominalCase_Using_Remote()
     {
+        var pairs = new string[] { "BTC-PERP", "ETH/USDT" };
+        _pairFilterLoaderMock.Object.AddAll(string.Join(";", pairs));
+        
         using var service = _container.GetInstance<GatherGroupedOrderBookService>();
         using var cts = new CancellationTokenSource(60 * 1000);
         await service.CollectOrderBooks(cts.Token);
 
-        var replayMemory = GroupedObHandlerMock.Object.ReplayMemory;
+        var replayMemory = _groupedObHandlerMock.Object.ReplayMemory;
         
-        Assert.NotEmpty(replayMemory);
+        Assert.NotEmpty(pairs);
+        Assert.Single(replayMemory);
         Assert.NotEmpty(replayMemory[0]);
-        Assert.NotEmpty(replayMemory[0].Where(details => details.Market == "ETH/USDT"));
-        Assert.NotEmpty(replayMemory[0].Where(details => details.Market == "BTC-PERP"));
-        
-        PairFilterLoaderMock.Verify(loader => loader.GetPairFilterAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.AtLeastOnce);
-        PairFilterLoaderMock.VerifyNoOtherCalls();
-        
-        
+        foreach (var pair in pairs)
+        {
+            Assert.NotEmpty(replayMemory[0].Where(details => details.Market == pair));
+        }
+
+        Assert.Equal(pairs.Length, replayMemory[0].Count);
+
+        _pairFilterLoaderMock.Verify(
+            loader => loader.GetPairFilterAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.AtLeastOnce);
+        _pairFilterLoaderMock.VerifyNoOtherCalls();
+
+        _groupedObHandlerMock.Verify(
+            handler => handler.Handle(It.IsAny<IReadOnlyCollection<GroupedOrderbookDetails>>(),
+                It.IsAny<CancellationToken>()), Times.Once);
+        _groupedObHandlerMock.VerifyNoOtherCalls();
     }
-    
 }
