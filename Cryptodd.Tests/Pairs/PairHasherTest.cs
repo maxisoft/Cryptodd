16a498c71764db9f26a10af21be8a2085ea1cb39
maxisoft <maxisoft.eu@gmail.com>
2022-06-03 10:14:39 +0200

    PairHasher to transform pair string into positive int64

---
+++ Cryptodd.Tests/Pairs/PairHasherTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Cryptodd.Ftx;
+using Cryptodd.Http;
+using Cryptodd.Pairs;
+using xRetry;
+using Xunit;
+
+namespace Cryptodd.Tests.Pairs;
+
+public class PairHasherTest
+{
+    [Fact]
+    public void TestHashPreconditions()
+    {
+        Assert.Equal(SHA256.HashData(Encoding.UTF8.GetBytes("test")).Length, PairHasher.Sha256ByteCount);
+        Assert.True(PairHasher.Sha256ByteCount / sizeof(long) > 0);
+        Assert.True(PairHasher.Sha256ByteCount % sizeof(long) == 0);
+        Assert.True(BitConverter.IsLittleEndian);
+    }
+    
+    [Fact]
+    public void TestHash()
+    {
+        Assert.Equal(0, PairHasher.Hash(""));
+        Assert.Equal(7966697908167619883, PairHasher.Hash("BTC-PERP"));
+        Assert.Equal(3846482641729352903, PairHasher.Hash("ETH-PERP"));
+
+        Parallel.For(0, 128, _ =>
+        {
+            Assert.Equal(0, PairHasher.Hash(""));
+            Assert.Equal(7966697908167619883, PairHasher.Hash("BTC-PERP"));
+            Assert.Equal(3846482641729352903, PairHasher.Hash("ETH-PERP"));
+        });
+        
+        Assert.True(PairHasher.Hash("LONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONGLONG") > 0);
+    }
+    
+    [RetryFact]
+    public async Task TestRealHash()
+    {
+        using var httpclient = new HttpClient();
+        var markets = await new FtxPublicHttpApi(httpclient, new UriRewriteService()).GetAllMarketsAsync();
+        Assert.NotEmpty(markets);
+        var marketsUnique = markets.Select(market => market.Name).Where(s => !string.IsNullOrEmpty(s)).ToImmutableHashSet();
+
+        var hashes = new ConcurrentBag<long>();
+        Parallel.ForEach(marketsUnique, s => hashes.Add(PairHasher.Hash(s)));
+
+        Assert.Equal(marketsUnique.Count, hashes.ToImmutableHashSet().Count);
+        foreach (var hash in hashes)
+        {
+            Assert.True(hash > 0);
+        }
+    }
+}
---
+++ Cryptodd/Pairs/PairHasher.cs
@@ -0,0 +1,77 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using Cryptodd.IoC;
+using Lamar;
+
+namespace Cryptodd.Pairs;
+
+[Singleton]
+public static class PairHasher
+{
+    internal const int Sha256ByteCount = 256 / sizeof(byte) / 8;
+    internal const ulong Salt = 18446744073709551557ul;
+    private static readonly MemoryPool<byte> MemoryPool = MemoryPool<byte>.Shared;
+
+
+    public static long Hash(string pair)
+    {
+        if (string.IsNullOrEmpty(pair))
+        {
+            return 0;
+        }
+
+        Span<byte> encoded = stackalloc byte[32];
+        Span<byte> destination = stackalloc byte[Sha256ByteCount];
+        var byteCount = Encoding.UTF8.GetByteCount(pair);
+        var res = byteCount >= encoded.Length
+            ? HashWithAlloc(pair, byteCount, destination)
+            : DoHash(pair, byteCount, encoded, destination);
+        return res;
+    }
+
+    private static long HashWithAlloc(string pair, int byteCount, Span<byte> result)
+    {
+        using var encodedMemory = MemoryPool.Rent(byteCount);
+
+        return DoHash(pair, byteCount, encodedMemory.Memory.Span, result);
+    }
+
+    private static long DoHash(string pair, int byteCount, Span<byte> buffer, Span<byte> destination)
+    {
+        var encodedBytes = Encoding.UTF8.GetBytes(pair, buffer);
+
+        if (encodedBytes != byteCount)
+        {
+            throw new ArgumentException("invalid byte count", nameof(byteCount));
+        }
+
+        if (!SHA256.TryHashData(buffer[..encodedBytes], destination, out var bytesWritten) ||
+            bytesWritten != Sha256ByteCount)
+        {
+            throw new ArgumentException("invalid byte count", nameof(pair));
+        }
+
+        Debug.Assert(destination.Length == bytesWritten);
+
+        var asLong = MemoryMarshal.Cast<byte, long>(destination);
+        var res = Salt;
+
+        unchecked
+        {
+            foreach (var l in asLong)
+            {
+                res ^= (ulong)IPAddress.HostToNetworkOrder(l);
+            }
+
+            res &= ~(1ul << 63); // remove sign bit 
+        }
+
+        Debug.Assert((long)res >= 0);
+
+        return (long)res;
+    }
+}
