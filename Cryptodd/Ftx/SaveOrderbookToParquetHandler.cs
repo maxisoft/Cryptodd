b3454d159511a41a1a38521a072f0063d6dc224e
maxisoft <maxisoft.eu@gmail.com>
2022-06-05 11:20:58 +0200

    Chunk SaveToParquet calls Added back all columns

--- Cryptodd/Ftx/SaveOrderbookToParquetHandler.cs
+++ Cryptodd/Ftx/SaveOrderbookToParquetHandler.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Cryptodd.FileSystem;
 using Cryptodd.Ftx.Models;
+using Cryptodd.Pairs;
 using Microsoft.Extensions.Configuration;
 using Parquet;
 using Parquet.Data;
@@ -17,20 +18,35 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
 {
     private readonly IConfiguration _configuration;
     private readonly IPathResolver _pathResolver;
+    private readonly IPairFilterLoader _pairFilterLoader;
+    private readonly int ChunkSize = 64;
 
-    public SaveOrderbookToParquetHandler(IConfiguration configuration, IPathResolver pathResolver)
+    public SaveOrderbookToParquetHandler(IConfiguration configuration, IPathResolver pathResolver,
+        IPairFilterLoader pairFilterLoader)
     {
         _configuration = configuration;
         _pathResolver = pathResolver;
+        _pairFilterLoader = pairFilterLoader;
     }
 
-    public Task Handle(IReadOnlyCollection<GroupedOrderbookDetails> orderbooks, CancellationToken cancellationToken)
+    public async Task Handle(IReadOnlyCollection<GroupedOrderbookDetails> orderbooks,
+        CancellationToken cancellationToken)
     {
-        Debug.Assert(!Disabled);
+        if (!orderbooks.Any())
+        {
+            return;
+        }
+
+        Debug.Assert(!Disabled, "Disabled state must be checked externally");
+        if (Disabled)
+        {
+            return;
+        }
+
         var section = _configuration.GetSection("Ftx").GetSection("GroupedOrderBook").GetSection("Parquet");
         if (!section.GetValue("Enable", false))
         {
-            return Task.CompletedTask;
+            return;
         }
 
         var fileName = section.GetValue<string>("File", "ftx_grouped_orderbook.parquet");
@@ -42,7 +58,21 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
                                  FileIntendedAction.Write
             });
         var gzip = section.GetValue("HighCompression", false); // use snappy else
-        return Task.Factory.StartNew(() => SaveToParquet(orderbooks, fileName, gzip), cancellationToken);
+        var pairFilter = await _pairFilterLoader.GetPairFilterAsync("Ftx.GroupedOrderBook.Parquet", cancellationToken)
+            .ConfigureAwait(false);
+
+        foreach (var array in orderbooks
+                     .Where(details => pairFilter.Match(details.Market))
+                     .Chunk(section.GetValue("ChunkSize", ChunkSize)))
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+
+            await Task.Factory.StartNew(() => SaveToParquet(array, fileName, gzip), cancellationToken)
+                .ConfigureAwait(false);
+        }
     }
 
     public bool Disabled { get; set; }
@@ -51,12 +81,12 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
         bool gzip)
     {
         var timeColumn = new DataField("time", DataType.Int64, false);
-        //var marketColumn = new DataField("market", DataType.String, false);
+        var marketColumn = new DataField("market", DataType.String, false);
         var groupingColumn = new DataField("grouping", DataType.Double, false);
         var bidsColumn = new DataField("bid", DataType.Double, isArray: true);
-        //var asksColumn = new DataField("ask", DataType.Double, isArray: true);
+        var asksColumn = new DataField("ask", DataType.Double, isArray: true);
 
-        var schema = new Schema(timeColumn, groupingColumn, bidsColumn);
+        var schema = new Schema(timeColumn, marketColumn, groupingColumn, bidsColumn, asksColumn);
 
         var exists = File.Exists(fileName);
         using Stream fileStream = File.Open(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
@@ -70,7 +100,7 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
         using var groupWriter = parquetWriter.CreateRowGroup();
         groupWriter.WriteColumn(new DataColumn(timeColumn,
             orderbooks.Select(o => o.Time.ToUnixTimeMilliseconds()).ToArray()));
-        //groupWriter.WriteColumn(new DataColumn(marketColumn, orderbooks.Select(o => o.Market).ToArray()));
+        groupWriter.WriteColumn(new DataColumn(marketColumn, orderbooks.Select(o => o.Market).ToArray()));
         groupWriter.WriteColumn(new DataColumn(groupingColumn, orderbooks.Select(o => o.Grouping).ToArray()));
         {
             var (bids, bidsRepetitionLevels) = PackPairs<PairBidSelector>(orderbooks);
@@ -79,12 +109,12 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
                 bidsRepetitionLevels));
         }
 
-        /*{
+        {
             var (asks, asksRepetitionLevels) = PackPairs<PairAskSelector>(orderbooks);
             groupWriter.WriteColumn(new DataColumn(asksColumn,
                 asks,
                 asksRepetitionLevels));
-        }*/
+        }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
@@ -107,10 +137,12 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
             {
                 payloadSpan[i] = span[i];
             }
+
             if (span.IsEmpty)
             {
                 payloadSpan[0] = null;
             }
+
             payloadSpan = payloadSpan[Math.Max(span.Length, 1)..];
             repetitionsSpan[0] = 0;
             repetitionsSpan = repetitionsSpan[Math.Max(span.Length, 1)..];
@@ -118,7 +150,7 @@ public class SaveOrderbookToParquetHandler : IGroupedOrderbookHandler
 
         Debug.Assert(repetitionsSpan.IsEmpty);
         Debug.Assert(payloadSpan.IsEmpty);
-        
+
         return (payload, repetitions);
     }
 
