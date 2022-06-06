3cdc7457dba36867a38e69c25e0961618a1fddf3
maxisoft <maxisoft.eu@gmail.com>
2022-06-06 20:24:54 +0200

    Use pathResolver to resolve sqlite database path Sqlite IDatabase is not a singleton to allow one to change the path at runtime

--- Cryptodd/IoC/Registries/Customs/SqliteDatabaseRegistry.cs
+++ Cryptodd/IoC/Registries/Customs/SqliteDatabaseRegistry.cs
@@ -1,4 +1,5 @@
-﻿using Lamar;
+﻿using Cryptodd.FileSystem;
+using Lamar;
 using Microsoft.Extensions.Configuration;
 using PetaPoco;
 using PetaPoco.Providers;
@@ -7,14 +8,24 @@ namespace Cryptodd.IoC.Registries.Customs;
 
 public class SqliteDatabaseRegistry: ServiceRegistry
 {
+    public const string DatabaseFileName = "cryptodd.sqlite";
+    public const string FileType = "sqlite3";
     public SqliteDatabaseRegistry()
     {
-        ForSingletonOf<IDatabase>().Add(context =>
+        For<IDatabase>().Add(context =>
         {
             var configuration = context.GetInstance<IConfiguration>();
             var postgresSection = configuration.GetSection("Postgres");
             var basePath = configuration.GetValue<string>("BasePath");
-            var defaultConnectionString = $"Data Source={Path.Combine(basePath, "cryptodd.db")};";
+            var pathResolver = context.GetInstance<IPathResolver>();
+            var dbPath = pathResolver.Resolve(DatabaseFileName,
+                new ResolveOption
+                {
+                    Namespace = GetType().Namespace!, FileType = FileType,
+                    IntendedAction = FileIntendedAction.Append | FileIntendedAction.Read | FileIntendedAction.Create |
+                                     FileIntendedAction.Write
+                });
+            var defaultConnectionString = $"Data Source={dbPath};";
             var res = DatabaseConfiguration.Build()
                 .UsingConnectionString(postgresSection.GetValue<string>("ConnectionString", defaultConnectionString))
                 .UsingProvider<SQLiteDatabaseProvider>()
