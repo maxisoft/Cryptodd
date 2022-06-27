82bf4d65d30ded2d04b7a1e5988d6e6b9ac7b7ca
maxisoft <maxisoft.eu@gmail.com>
2022-06-27 14:39:39 +0200

    Refactor SaveOrderbookToDatabase to use db transaction

--- Cryptodd/Bitfinex/Orderbooks/SaveOrderbookToDatabase.cs
+++ Cryptodd/Bitfinex/Orderbooks/SaveOrderbookToDatabase.cs
@@ -4,6 +4,7 @@ using Cryptodd.Databases;
 using Cryptodd.Databases.Tables.Bitfinex;
 using Cryptodd.Features;
 using Lamar;
+using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 using NpgsqlTypes;
 using PetaPoco.SqlKata;
@@ -30,6 +31,7 @@ public class SaveOrderbookToDatabase : IOrderbookHandler
         {
             return;
         }
+
         await using var container = _container.GetNestedContainer()!;
         await using var connection = container.GetInstance<NpgsqlConnection>();
         if (connection is not null)
@@ -58,23 +60,36 @@ public class SaveOrderbookToDatabase : IOrderbookHandler
             await tableService.CreateTableIfNotExists<OrderbookTableSchema>(table, connection, cancellationToken)
                 .ConfigureAwait(false);
             await Reconnect(connection).ConfigureAwait(false);
-
-            await using var writer =
-                await connection.BeginBinaryImportAsync(
-                    $"COPY {table.Schema}.\"{table.Table}\" FROM STDIN (FORMAT BINARY)", cancellationToken);
-            await writer.StartRowAsync(cancellationToken);
-            await writer.WriteAsync(orderbook.Time, NpgsqlDbType.Bigint, cancellationToken);
-            for (var index = 0; index < orderbook.Orderbook.Count; index++)
-            {
-                var ob = orderbook.Orderbook[index];
-                // ReSharper disable MethodHasAsyncOverloadWithCancellation
-                writer.Write((float)ob.Price, NpgsqlDbType.Real);
-                writer.Write(ob.Count, NpgsqlDbType.Integer);
-                writer.Write((float)ob.Size, NpgsqlDbType.Real);
-                // ReSharper restore MethodHasAsyncOverloadWithCancellation
-            }
-
-            await writer.CompleteAsync(cancellationToken);
+            await using var tr =
+                await connection.BeginTransactionAsync(IsolationLevel.ReadCommitted, cancellationToken);
+            await ImportOrderbook(connection, table, orderbook, cancellationToken);
+            await tr.CommitAsync(cancellationToken);
         }
+    }
+
+    private static async Task ImportOrderbook(NpgsqlConnection connection, OrderbookTableSchema table,
+        OrderbookEnvelope orderbook, CancellationToken cancellationToken)
+    {
+        await using var writer =
+            await connection.BeginBinaryImportAsync(
+                $"COPY \"{table.Schema}\".\"{table.Table}\" FROM STDIN (FORMAT BINARY)", cancellationToken);
+        await writer.StartRowAsync(cancellationToken);
+        await writer.WriteAsync(orderbook.Time, NpgsqlDbType.Bigint, cancellationToken);
+
+        static void WriteTuples(OrderbookEnvelope orderbookEnvelope, NpgsqlBinaryImporter npgsqlBinaryImporter)
+        {
+            foreach (ref var ob in orderbookEnvelope.Orderbook)
+            {
+                // ReSharper disable MethodHasAsyncOverloadWithCancellation
+                npgsqlBinaryImporter.Write((float)ob.Price, NpgsqlDbType.Real);
+                // ReSharper disable once RedundantCast
+                npgsqlBinaryImporter.Write((int)ob.Count, NpgsqlDbType.Integer);
+                npgsqlBinaryImporter.Write((float)ob.Size, NpgsqlDbType.Real);
+            }
+        }
+
+        WriteTuples(orderbook, writer);
+
+        await writer.CompleteAsync(cancellationToken);
     }
 }
