e3641c30f6586ff0222a3f4d05efee3309f49633
maxisoft <maxisoft.eu@gmail.com>
2022-12-08 11:30:20 +0100

    Moved BitfinexGroupedOrderbookTaskP0 to it's own file

--- Cryptodd/Scheduler/Tasks/Bitfinex/BitfinexGroupedOrderbookTask.cs
+++ Cryptodd/Scheduler/Tasks/Bitfinex/BitfinexGroupedOrderbookTask.cs
@@ -111,15 +111,3 @@ public class BitfinexGroupedOrderbookTask : BasePeriodicScheduledTask
         base.Dispose(disposing);
     }
 }
-
-public class BitfinexGroupedOrderbookTaskP0 : BitfinexGroupedOrderbookTask
-{
-    protected override BitfinexGatherGroupedOrderBookService GetOrderBookService() =>
-        Container.GetInstance<BitfinexGatherGroupedOrderBookServiceP0>();
-
-    public BitfinexGroupedOrderbookTaskP0(IContainer container, ILogger logger, IConfiguration configuration) : base(
-        container, logger, configuration) { }
-    
-    public override IConfigurationSection Section =>
-        Configuration.GetSection("Bitfinex").GetSection("OrderBookP0").GetSection("Task");
-}
