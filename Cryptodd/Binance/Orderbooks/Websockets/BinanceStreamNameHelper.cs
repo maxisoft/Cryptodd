796577d7a294ab2c619a688275b8d252d038e8ea
maxisoft <maxisoft.eu@gmail.com>
2022-12-14 12:36:37 +0100

    Faster regex

--- Cryptodd/Binance/Orderbooks/Websockets/BinanceStreamNameHelper.cs
+++ Cryptodd/Binance/Orderbooks/Websockets/BinanceStreamNameHelper.cs
@@ -5,9 +5,13 @@ namespace Cryptodd.Binance.Orderbooks.Websockets;
 
 public static partial class BinanceStreamNameHelper
 {
-    [GeneratedRegex(@"@depth(?>@\d+m?s)?$", RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    [GeneratedRegex(@"@depth(?>(?>@[0-9]+?m?s)?)$", RegexOptions.Singleline | RegexOptions.CultureInvariant)]
     private static partial Regex DepthRegex();
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool IsDepth(ReadOnlySpan<char> stream) => DepthRegex().IsMatch(stream);
+    public static bool IsDepth(ReadOnlySpan<char> stream)
+    {
+        var start = stream.IndexOf("@depth");
+        return start > 0 && DepthRegex().IsMatch(stream[start..]);
+    }
 }
