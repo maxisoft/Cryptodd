7a4680edbfdcb1cc7f2db670a70df15490aff9ba
maxisoft <maxisoft.eu@gmail.com>
2022-12-14 12:25:45 +0100

    It seems RegexOptions.NonBacktracking is not supported by the regex source generator according to https://devblogs.microsoft.com/dotnet/regular-expression-improvements-in-dotnet-7/

--- Cryptodd/Binance/Orderbooks/Websockets/BinanceStreamNameHelper.cs
+++ Cryptodd/Binance/Orderbooks/Websockets/BinanceStreamNameHelper.cs
@@ -1,11 +1,13 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace Cryptodd.Binance.Orderbooks.Websockets;
 
 public static partial class BinanceStreamNameHelper
 {
-    [GeneratedRegex(@"@depth(?:@\d+m?s)?$", RegexOptions.Singleline | RegexOptions.NonBacktracking)]
+    [GeneratedRegex(@"@depth(?>@\d+m?s)?$", RegexOptions.Singleline | RegexOptions.CultureInvariant)]
     private static partial Regex DepthRegex();
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsDepth(ReadOnlySpan<char> stream) => DepthRegex().IsMatch(stream);
 }
