b81fb59b282769c633c8b5e7909b2a44e567694d
maxisoft <maxisoft.eu@gmail.com>
2023-02-02 18:35:40 +0100

    Added Deconstruct method to OkxOptionInstrumentId

--- Cryptodd/Okx/Options/OkxOptionInstrumentId.cs
+++ Cryptodd/Okx/Options/OkxOptionInstrumentId.cs
@@ -12,10 +12,21 @@ public readonly record struct OkxOptionInstrumentId(
 )
 {
     public static OkxInstrumentType InstrumentType => OkxInstrumentType.Option;
+
     public DateOnly Date => new(2000 + Year, Month, Day);
+
     public bool IsPut => Side is OkxOptionSide.Put;
+
     public bool IsCall => Side is OkxOptionSide.Call;
 
     public static bool TryParse(string value, out OkxOptionInstrumentId instrumentId) =>
         OkxOptionInstrumentIdParser.TryParse(value, out instrumentId);
+
+    public void Deconstruct(out string underlying, out DateOnly date, out double price, out OkxOptionSide side)
+    {
+        underlying = Underlying;
+        date = Date;
+        price = Price;
+        side = Side;
+    }
 }
