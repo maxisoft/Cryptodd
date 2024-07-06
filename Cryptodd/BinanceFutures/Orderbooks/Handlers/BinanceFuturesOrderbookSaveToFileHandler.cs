1630a55f241c84b289f272ecddbfd7c0586dec96
maxisoft <maxisoft.eu@gmail.com>
2024-07-06 11:15:52 +0200

    refactor: Replace BinanceOrderbookSaveToFileHandler with BinanceFuturesOrderbookSaveToFileHandler

---
+++ Cryptodd/BinanceFutures/Orderbooks/Handlers/BinanceFuturesOrderbookSaveToFileHandler.cs
@@ -0,0 +1,9 @@
+﻿using Cryptodd.Binance.Orderbooks.Handlers;
+using Cryptodd.IoC;
+
+namespace Cryptodd.BinanceFutures.Orderbooks.Handlers;
+
+// ReSharper disable once UnusedType.Global
+public sealed class BinanceFuturesOrderbookSaveToFileHandler(BinanceFuturesOrderBookWriter writer) :
+    BaseBinanceOrderbookSaveToFileHandler<BinanceFuturesOrderBookWriter, BinanceFuturesOrderBookWriterOptions>(writer),
+    IBinanceFuturesAggregatedOrderbookHandler, IService;
